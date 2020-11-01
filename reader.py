828b31ce5b08eb239831ea6deafa11450c729357
Ezra Citron <citron.ezra@gmail.com>
2020-10-31 19:20:17 -0700

    initial commit; add function names

---
+++ reader.py
@@ -0,0 +1,35 @@
+datadir = 'data/'
+infile = 'file_inputs.txt'
+
+with open(datadir+infile, 'r') as file:
+    # read all lines into list of strings
+    content = file.read().split('\n')
+
+
+def process_line():
+    """Process an individual line from input."""
+    pass
+
+
+def create_parking_lot():
+    """Create a parking lot."""
+    pass
+
+
+def park():
+    """Park a car."""
+    pass
+
+
+def registration_numbers_for_cars_with_color():
+    """TODO"""
+    pass
+
+
+def slot_numbers_for_cars_with_color():
+    """TODO"""
+    pass
+
+
+def status():
+    pass
